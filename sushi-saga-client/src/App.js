43005ff8d3f83a6383ab865e7723e58c0e65ae3d
hasibulc <hasibulc91@gmail.com>
2020-01-14 22:59:22 -0500

    done

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,35 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushiArray: [],
+    eaten: {},
+    custMoney: 100
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushiArray => this.setState({sushiArray}))
+  }
+
+  handleEaten = (id, price) => {
+    if ( !this.state.eaten[id] && price <= this.state.custMoney ){
+      this.setState({ eaten: { ...this.state.eaten, [id]: true }, custMoney: this.state.custMoney - price })
+    }
+  }
+
   render() {
+
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer 
+          sushiArray={this.state.sushiArray} 
+          handleEaten={this.handleEaten} 
+          eaten={this.state.eaten}/>
+        <Table 
+          emptyPlates={Object.keys(this.state.eaten)}
+          custMoney={this.state.custMoney}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,42 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
+class SushiContainer extends React.Component {
+
+  state= {
+    startIndex: 0,
+    endIndex: 4
+  }
+
+  handleMoreButton = () => {
+    this.setState({ startIndex: this.state.startIndex + 4});
+    this.setState({ endIndex: this.state.endIndex + 4});
+  }
+
+  render() {
+    
+    return (
+      <Fragment>
+        <div className="belt">
+          {
+            /* 
+              Render Sushi components here!
+            */
+          this.props.sushiArray.slice(this.state.startIndex, this.state.endIndex).map((sushi) => <Sushi 
+            sushi={sushi} 
+            key={sushi.id} 
+            handleEaten={this.props.handleEaten} 
+            eaten={this.props.eaten[sushi.id]}/>)
+          }
+          <MoreButton 
+            handleMoreButton={this.handleMoreButton}/>
+        </div>
+      </Fragment>
+    )
+
+  }
+
 }
 
 export default SushiContainer
